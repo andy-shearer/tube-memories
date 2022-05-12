e57a6c2fe25d772e12be9dd73b9f62dc8b2dc0a0
Andy Shearer <andyshearer.95@googlemail.com>
2022-05-12 14:34:35 +0100

    Deploy to Rinkeby, add constants to front-end

--- contract/contracts/TubeMemories.sol
+++ contract/contracts/TubeMemories.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
--- site/pages/index.js
+++ site/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { ABI, NFT_CONTRACT_ADDRESS } from "../constants";
 
 export default function Home() {
   return (
